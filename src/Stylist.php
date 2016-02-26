e7af606727c95ee23ed514f52d995560f1920e3b
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-26 08:41:49 -0800

    add class stylist functions

---
+++ src/Stylist.php
@@ -0,0 +1,65 @@
+<?php
+    class Stylist
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id=null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stylists (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+            $stylists = array();
+            foreach ($returned_stylists as $stylist) {
+                $name = $stylist['name'];
+                $id = $stylist['id'];
+                $new_stylist = new Stylist($name, $id);
+                array_push($stylists, $new_stylist);
+            }
+            return $stylists;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->query("DELETE FROM stylists;");
+
+        }
+
+        static function find($search_id)
+        {
+            $found_stylist = NULL;
+            $stylists = Stylist::getAll();
+            foreach ($stylists as $stylist) {
+                if ($stylist->getId() == $search_id) {
+                    $found_stylist = $stylist;
+                }
+            }
+            return $found_stylist;
+        }
+    }
+ ?>
