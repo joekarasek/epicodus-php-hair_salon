1113610f3454c36517ca3c81f8e8135c7cd626b0
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-26 09:07:58 -0800

    link app.php to db

--- app/app.php
+++ app/app.php
@@ -1,10 +1,19 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
 
+    // create silex object with twig templating
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
+
+    // setup server for database
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
